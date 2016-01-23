00accf189de77327c76d95a12e9dbfe71026db7b
dis-zephyro <zephyro@yandex.ru>
2016-01-23 04:39:23 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+$('.teachers-slider').slick({
+    arrows: true,
+    autoplay: false,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<span class="teacher-nav prev">',
+    nextArrow: '<span class="teacher-nav next">'
+});
